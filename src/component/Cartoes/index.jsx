841030514e4334f5b1df007d2c547b8a27876c17
Matheus Macedo <matheusg3.201168@gmail.com>
2023-04-06 22:29:48 -0300

    added ternary operator in card component

--- src/component/Cartoes/index.jsx
+++ src/component/Cartoes/index.jsx
@@ -2,8 +2,8 @@ import Frontcartao from "../../images/bg-card-front.png";
 import Backcard from "../../images/bg-card-back.png";
 
 export const Cartoes = (props) => {
-  const date = props.inputs
-  
+  const date = props.inputs;
+
   return (
     <div className="bg-bgMobile bg-cover bg-no-repeat md:min-h-screen md:w-[480px]">
       <div className="flex flex-col max-w-[350px] m-auto relative bottom-[-50px] md:margin-0 md:bottom-0 md:min-h-screen md:justify-center md:gap-[30px] md:max-w-[100%] md:left-[100px]">
@@ -14,19 +14,20 @@ export const Cartoes = (props) => {
         />
 
         <div className="z-10 relative self-start md:self-end">
-          <img className="
-            w-[300px] shadow-lg md:w-[350px]" 
-            src={Frontcartao} 
-            alt="cartão" 
+          <img
+            className="
+            w-[300px] shadow-lg md:w-[350px]"
+            src={Frontcartao}
+            alt="cartão"
           />
 
           <p className="text-white absolute z-20 text-[23px] bottom-[60px] left-[20px] tracking-[1px]">
-           {date.number}
+            {date.number === '' ? '0000 0000 0000 0000': date.number}
           </p>
 
           <div className="flex absolute bottom-[20px] left-[20px] z-10 text-white w-full gap-[34%] text-[11px] tracking-[1px]">
-            <p>{date.name}</p>
-            <p>{date.date}</p>
+            <p>{date.name === '' ? 'Matheus M': date.name}</p>
+            <p>{date.date === '' ? '16/12': date.date}</p>
           </div>
         </div>
       </div>
